450964c8ca82951008e22a1f29305bc341125842
Matteo Tinta <matteo.tinta.external@yoox.com>
2022-09-29 16:47:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node Bootstrapper Typescript
+
+This repo contains a super base bootstrap with node.js and typescript!
+
+happy build!
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const config = {
+    verbose: true,
+    collectCoverage: true,
+    coverageReporters: ['lcov', 'json']
+  };
+  
+  module.exports = config;
