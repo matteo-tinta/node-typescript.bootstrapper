1cad5483d0d779807e96bd451a4163844eddccb7
Matteo Tinta <matteo.tinta.external@yoox.com>
2022-09-29 17:49:52 +0200

    Updated with configuration for node

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,39 @@
 const path = require('path');
-const { NODE_ENV = 'production' } = process.env;
+const nodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+var nodeExternals = require('webpack-node-externals');
 
-let configs = {
-    entry: './src/index.ts',
-    mode: NODE_ENV,
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-            test: /\.ts$/,
-                use: [
-                    'ts-loader',
-                ]
-            }
-        ]
-    }
-}
-
-module.exports = configs
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  mode: 'production',
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  externalsPresets: {
+    node: true, // in order to ignore built-in modules like path, fs, etc.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: ['node_modules'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [new nodePolyfillPlugin()],
+};
